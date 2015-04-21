eed328218aa39eab80e887291ee1e32415dfbc4b
Stran6 <st.tran6@gmail.com>
2015-04-21 15:21:42 -0600

    Completed (?) Install.js

--- src/install.js
+++ src/install.js
@@ -1,5 +1,16 @@
 import fs from 'fs';
+import path from 'path';
 
-console.log(__DIRNAME);
+let currentPath = process.env.pwd;
 
-//fs.createReadStream('../hooks/commit-msg').pipe(fs.createWriteStream())
+while(fs.existsSync(currentPath)) {
+	if(fs.existsSync(currentPath + '/.git'))
+	{
+		currentPath = currentPath + '/.git/hooks/';
+		break;
+	}
+	else
+		currentPath = path.normalize(currentPath + '/..');
+}
+
+fs.createReadStream('hooks/commit-msg').pipe(fs.createWriteStream(currentPath + 'commit-msg'));
