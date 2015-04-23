c4062c437fe1cb102f6629782b4c8a9d8e0e141e
Matthew Radcliffe <mradcliffe@student.neumont.edu>
2015-04-23 13:42:22 -0600

    Minor Fixes
    Remove commented out code.
    Remove windows specific file path seperators.
    Move pre-commit hook tests from placeholder-test.js to
    precommit-hook-test.js

--- src/install.js
+++ src/install.js
@@ -13,13 +13,13 @@ export function findGitFolder(startDirectory) {
 	let gitPath = startDirectory;
 
 	while(fs.existsSync(gitPath)) {
-		if(fs.existsSync(gitPath + '\\.git')) {
-			gitPath = gitPath + '\\.git';
+		if(fs.existsSync(path.join(gitPath, '/.git'))) {
+			gitPath = path.join(gitPath, '/.git');
 			break;
 		}
 		else {
 			let tempPath = gitPath;
-			gitPath = path.normalize(gitPath + '\\..');
+			gitPath = path.normalize(path.join(gitPath, '/..'));
 
 			if(gitPath === tempPath) {
 				throw new Error('Cannot find Git Folder!!');
@@ -31,6 +31,6 @@ export function findGitFolder(startDirectory) {
 }
 
 export function copyHookFiles(gitDirectory) {
-	fs.createReadStream(path.resolve(path.join(__dirname, '..\\hooks\\commit-msg')))
-	.pipe(fs.createWriteStream(gitDirectory + '\\hooks\\commit-msg'));
+	fs.createReadStream(path.resolve(path.join(__dirname, '../hooks/commit-msg')))
+	.pipe(fs.createWriteStream(path.join(gitDirectory, '/hooks/commit-msg')));
 }
