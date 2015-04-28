9f2dfa4e1d7603101df4372fac45958d7cc69113
Matthew Radcliffe <mradcliffe@student.neumont.edu>
2015-04-28 16:22:42 -0600

    Parse issue numbers form commit messages working

--- src/precommit-entry.js
+++ src/precommit-entry.js
@@ -1,4 +1,30 @@
 import fsp from 'fs-promise';
-export default function precommit (path) {
+
+export function getCommitMsg (path) {
   return fsp.readFile(path, {encoding: 'utf8'});
+}
+
+export function getIssueReference(msgToParse, prjKey) {
+  let pattern = new RegExp(`${prjKey}-\\d+`, 'g');
+  let commentPattern = RegExp(`#.*`, 'g');
+
+  msgToParse = msgToParse.replace(commentPattern, '');
+
+  let refrences = msgToParse.match(pattern);
+  let unique = null;
+
+  if(refrences !== null) {
+    unique = refrences.slice()
+      .sort((a, b) => {
+        return a - b;
+      })
+      .reduce((a, b) => {
+        if (a.slice(-1)[0] !== b) {
+          a.push(b);
+        }
+        return a;
+      }, []);
+  }
+
+  return unique;
 }
