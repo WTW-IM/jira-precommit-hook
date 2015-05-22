db5211103224336d4c7f229e3b06190ea163b004
Karl Thompson <kthompson@student.neumont.edu>
2015-05-22 16:17:21 -0600

    Clean excess promise-handling

--- src/issue-handler.js
+++ src/issue-handler.js
@@ -2,10 +2,5 @@ import strats from './validation-strategies/index';
 
 export function issueStrategizer(issues, jiraClientAPI) {
   return Promise.all(strats.map(s => s(issues, jiraClientAPI)))
-    .then(() => {
-      return Promise.resolve(true);
-    })
-    .catch(err => {
-      return Promise.reject(err);
-    });
+    .then(() => true);
 }
