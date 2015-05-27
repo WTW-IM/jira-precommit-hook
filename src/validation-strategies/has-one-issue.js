8e2749d3c29b0575d1937643b5311080bf67b360
Steven Tran <St.Tran6@gmail.com>
2015-05-27 15:10:52 -0600

    Allow strictSSL option to be specified
    -Change from defaulting option to false to using strictSSL field in
    .jirarc
    -One minor spelling fix

--- src/validation-strategies/has-one-issue.js
+++ src/validation-strategies/has-one-issue.js
@@ -1,6 +1,6 @@
 export default function apply(issues, jiraClientAPI) {
   if(issues.length === 0) {
-    return Promise.reject(new Error('Must commit agianst at least one issue.'));
+    return Promise.reject(new Error('Must commit against at least one issue.'));
   }
   return Promise.resolve(true);
 }
