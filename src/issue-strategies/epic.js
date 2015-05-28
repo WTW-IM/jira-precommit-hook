f883c057b76a862304c5d5831e575727a9b00744
Karl Thompson <kthompson@student.neumont.edu>
2015-05-28 14:09:46 -0600

    Fix typos in error-handling of issue-validation
    Add issue key to error message

--- src/issue-strategies/epic.js
+++ src/issue-strategies/epic.js
@@ -1,3 +1,3 @@
 export function apply(issue, jiraClientAPI) {
-  return Promise.reject(new Error('Cannot commit against against an epic.'));
+  return Promise.reject(new Error(`Cannot commit against ${issue.key}. It is an epic.`));
 }
