ae42b1bfd03c4ec13e2ea3f5542effe099c99edd
Darrius Wright <darrius.wright@extendhealth.com>
2015-09-09 07:38:17 -0600

    Removing lint issues from bug mantainance

--- src/issue-strategies/bug-maintenance.js
+++ src/issue-strategies/bug-maintenance.js
@@ -4,7 +4,7 @@ export function apply(issue, jiraClientAPI) {
   if(issue === null || issue.fields.status.statusCategory.colorName !== 'yellow') {
     return Promise.reject(new Error(`Issue ${issue.key} is not open to commit against`));
   }
-  
+
   if(!issue.fields.issuelinks)
   {
     return Promise.resolve(true);
@@ -14,8 +14,8 @@ export function apply(issue, jiraClientAPI) {
     if(!parent || parent.fields.status.statusCategory.colorName === 'yellow')
     {
       return Promise.resolve(true);
-    } 
-    
+    }
+
     return Promise.reject(new Error(`Cannot commit issue ${issue.key} because parent issue ${parent.key} is not available to commit against.`));
-  })
+  });
 }
