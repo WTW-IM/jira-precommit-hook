424b71460468bb3c0103c72fabe76e5a2f345da4
Matt Smith <mtscout6@gmail.com>
2016-03-20 20:50:58 -0600

    Use update-notifier for the update check to make hook faster

--- src/outdated-check.js
+++ src/outdated-check.js
@@ -1,14 +1,15 @@
 import 'colors';
 import pkg from '../package.json';
-import request from 'request-promise';
+import updateNotifier from 'update-notifier';
 
 export default async function checkOutdated() {
-  const rawResponse = await request(`https://registry.npmjs.org/${pkg.name}/latest`);
-  const result = JSON.parse(rawResponse);
-  if (pkg.version !== result.version) {
-    let message = `WARNING: You are using version ${pkg.version} of the jira-precommit-hook. `;
-    message += `However, version ${result.version} has been released. To update run:`.yellow;
-    message += '\n> '.grey + `npm install ${pkg.name}@${result.version} --save-dev\n`.green;
+  const notifier = updateNotifier({ pkg });
+
+  if (notifier.update) {
+    const { current, latest } = notifier.update;
+    let message = `WARNING: You are using version ${current} of the jira-precommit-hook. `.yellow;
+    message += `However, version ${latest} has been released. To update run:`.yellow;
+    message += '\n> '.grey + `npm install ${pkg.name}@${latest} --save-dev\n`.green;
     console.warn(message);
   }
 }
